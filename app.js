86f77ce4589fe2e0c0a7124d44b4263aacb29841
Infernocreator33 <zs0877310@otc.edu>
2018-10-29 10:28:38 -0500

    update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ console.log("Hello world");
 const chalk = require('chalk');
 const chalkAnimation = require('chalk-animation');
 console.log(chalk.blue("Hello world"));
+//will only work for some versions correctly in terminal
 console.log(chalk.keyword('orange')('Yay for orange colored text!'));
 console.log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'));
 console.log(chalk.hex('#DEADED').bold('Bold gray!'));
@@ -10,4 +11,4 @@ chalkAnimation.rainbow('Lorem Ipsum');
 setTimeout(() => {
     //stop the lorem ipsum animation, then write on a new line
     console.log('dolor sit amet');
-}, 1000);
+}, 100000000);
